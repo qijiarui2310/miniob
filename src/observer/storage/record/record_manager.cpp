5397ff6b34b6af8d42a2316a99d8f5879c2d9591
song junbao <3401107504@qq.com>
2024-07-16 12:34:36 +0000

    lab1

--- src/observer/storage/record/record_manager.cpp
+++ src/observer/storage/record/record_manager.cpp
@@ -417,6 +417,7 @@ bool RecordPageHandler::is_full() const { return page_header_->record_num >= pag
 
 RC PaxRecordPageHandler::insert_record(const char *data, RID *rid)
 {
+  std::lock_guard<std::mutex> lock(mutex);
   ASSERT(rw_mode_ != ReadWriteMode::READ_ONLY,
          "cannot insert record into page while the page is readonly");
 
